72f1205a8e78179c010a19ee0e0db1b620e82faf
Houssem Maamria <maamria.houssem@gmail.com>
2023-08-15 18:46:47 +0200

    change logo and description to login page, add desccription if page title

--- resources/views/back/volcator/elements/super/auth/login.blade.php
+++ resources/views/back/volcator/elements/super/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('elements.layouts.app')
 @section('content')
-    <div class="cui__layout">
+<div class="cui__layout">
     <div class="cui__layout__content">
         <div class="cui__utils__content">
             <div class="cui__auth__authContainer">
@@ -8,24 +8,24 @@
                     <div class="cui__auth__logoContainer">
                         <div class="cui__auth__logoContainer__logo">
                             <img src="{{ asset(app('back').'/assets/images/logos/volcator-logo.png') }}" class="mr-2 cui__logo_w70_h70" alt="Twedoo" />
-                            <div class="cui__auth__logoContainer__name">Twedoo</div>
+                            <div class="cui__auth__logoContainer__name">Volcator</div>
                         </div>
                     </div>
                     <div class="d-none d-sm-block">
                         <span>
                                 <a
-                                        href="javascript: void(0);"
-                                        style="bottom: calc(50% + 60px)"
-                                        class="cui__sidebar__toggleButton cui__sidebar__actionToggleTheme"
-                                        data-toggle="tooltip"
-                                        data-placement="left"
-                                        title="Switch Dark / Light Theme"
+                                    href="javascript: void(0);"
+                                    style="bottom: calc(50% + 60px)"
+                                    class="cui__sidebar__toggleButton cui__sidebar__actionToggleTheme"
+                                    data-toggle="tooltip"
+                                    data-placement="left"
+                                    title="Switch Dark / Light Theme"
                                 >
         <i class=" fe fe-moon cui__sidebar__on"></i>
         <i class="fe fe-sun cui__sidebar__off"></i>
     </a>
                         </span>
-                            <span class="dropdown mr-2 d-sm-block">
+                        <span class="dropdown mr-2 d-sm-block">
                                 <a href="" class="dropdown-toggle text-nowrap" data-toggle="dropdown" data-offset="5,15">
                                     <span class="dropdown-toggle-text">Language : <span class="text-uppercase">{{App::getLocale()}}</span></span>
                                 </a>
@@ -43,16 +43,16 @@
                     </div>
                 </div>
                 <div class="cui__auth__containerInner">
-                        <div class="text-center mb-5">
-                            <h1 class="mb-5 px-3 cui__auth__text__above__form__login">
-                                <strong>Welcome to {{ app('APP_NAME') }}</strong>
-                            </h1>
-                            <p class="mb-4">
-                                {{ app('APP_NAME') }} {{  trans('Configurations::Configurations/Configurations.description_app_stone_space')   }}
-                                <br />
-                                {{  trans('Configurations::Configurations/Configurations.description_app_stone_space_two')   }}
-                            </p>
-                        </div>
+                    <div class="text-center mb-5">
+                        <h1 class="mb-5 px-3 cui__auth__text__above__form__login">
+                            <strong>Welcome to {{ app('APP_NAME') }}</strong>
+                        </h1>
+                        <p class="mb-4">
+                            {{ app('APP_NAME') }} {{  trans('Configurations::Configurations/Configurations.description_app_volcator_space')   }}
+                            <br />
+                            {{  trans('Configurations::Configurations/Configurations.description_app_volcator_space_two')   }}
+                        </p>
+                    </div>
                     <div class="card cui__auth__boxContainer">
                         <div class="text-dark font-size-24 mb-4">
                             <strong class="text__color__stone-blue">Sign in </strong>
@@ -62,7 +62,7 @@
                             <div class="form-group mb-4 {{ $errors->has('email') ? ' has-error' : '' }}">
                                 <input type="email" class="form-control" placeholder="Email Address" name="email" value="{{ old('email') }}"/>
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
@@ -70,25 +70,25 @@
                             <div class="form-group mb-4 {{ $errors->has('password') ? ' has-error' : '' }}">
                                 <input type="password" class="form-control" placeholder="Password"  name="password" />
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
                             </div>
                             <div class="flex items-center">
-                                <a href="auth-forgot-password.html" class="kit__utils__link font-size-16">
+                                <a href="{{ url(app('urlBack').'/password/reset') }}" class="kit__utils__link font-size-16">
                                     {{ trans('Access-Controls::Access-Controls/Access-Controls.forget_password') }}
                                 </a>
                             </div>
-                           <button class="btn btn__primary__stone text-center w-30 pull-right mt-3">
+                            <button class="btn btn__primary__stone text-center w-30 pull-right mt-3">
                                 <strong>{{ trans('Access-Controls::Access-Controls/Access-Controls.login') }}</strong>
                             </button>
                         </form>
                     </div>
                     <div class="text-center pt-2 mb-auto">
-                        <span class="mr-2">Don't have an account?</span>
-                        <a href="{{ url(app('urlBack').'/password/reset') }}" class="kit__utils__link font-size-16">
-                        {{ trans('Access-Controls::Access-Controls/Access-Controls.create_account') }}
+                        <!--                        <span class="mr-2">Don't have an account?</span>-->
+                        <a href="#" class="kit__utils__link font-size-16">
+                            Create account through invitation.
                         </a>
                     </div>
                 </div>
@@ -108,16 +108,16 @@
                         </li>
                     </ul>
                     <div class="text-center">
-                            Copyright &copy; <script>document.write(new Date().getFullYear())</script> Volcatorspace.io |
-                            <a href="https://www.stonespace.io/privacy" target="_blank" rel="noopener noreferrer">
-                                Privacy Policy
-                            </a>
+                        Copyright &copy; <script>document.write(new Date().getFullYear())</script> |
+                        <a href="https://www.volcator.com" target="_blank" rel="noopener noreferrer">
+                            volcator.com
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    </div>
+</div>
 
 @endsection
--- src/Modules/Configurations/lang/en/Configurations/Configurations.php
+++ src/Modules/Configurations/lang/en/Configurations/Configurations.php
@@ -1,8 +1,9 @@
 <?php
 return [
     'Configurations' => 'Configurations',
+    'title_tab_description' => ' - Develop your application effortlessly',
     'description_app_volcator_space' => 'is a stack services platform for enterprise.',
-    'description_app_volcator_space_two' => 'Launch your business on one space and fall in love with multiple services.',
+    'description_app_volcator_space_two' => 'Build your dreams, effortlessly One click with Volcator',
     'agree_to' => 'I agree to',
     'terms_of_service' => 'terms of service',
 ];
